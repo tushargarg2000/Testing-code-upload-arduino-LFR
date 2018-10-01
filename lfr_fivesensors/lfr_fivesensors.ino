3a20c6d5cf33af1dd0002db3fa5ec73b2a56feb6
m1-key <39923784+m1-key@users.noreply.github.com>
2018-10-02 01:22:33 +0530

    Update lfr_fivesensors.ino

--- lfr_fivesensors/lfr_fivesensors.ino
+++ lfr_fivesensors/lfr_fivesensors.ino
@@ -1,24 +1,71 @@
+int mLf = 2  // for motor left
+int mLb = 3
+int mRf = 7  //for motor right
+int mRb = 8
 
 
-void setup()
-{
- pinMode(D4, OUTPUT);
+void setup() {
+ 
+ pinMode(mLf, OUTPUT); // for motors
+ pinMode(mLb, OUTPUT);
+ pinMode(mRf, OUTPUT);
+ pinMode(mRb, OUTPUT);
+   
+ pinMode(A0, INPUT); // for ELS,LS,MS,RS,ERS IR sensors in the strip
+ pinMode(A1, INPUT);
+ pinMode(A2, INPUT);
+ pinMode(A3, INPUT);
+ pinMode(A4, INPUT);
   
-Serial.begin(9600);
 }
 
+void loop() {
+  int ELS = analogRead(A0);  // taking the readings from different sensors
+  int LS =  analogRead(A1);
+  int MS =  analogRead(A2);
+  int RS =  analogRead(A3);
+  int ERS = analogRead(A4);
+  
 
-void loop()
-{
- int a = Serial.read()  - '0';
- if( a== 1)
- {
-  digitalWrite(D4 , LOW);
-  delay(500);
+ if(ELS > 500 and LS > 500 and MS < 500 and RS > 500 and ERS > 500) { // For straight line movement
+
+ digitalWrite(mLf , HIGH) ;
+ digitalWrite(mRf , HIGH) ;
+ digitalWrite(mLb, LOW) ;
+ digitalWrite(mRb, LOW) ;
+   
  }
- if( a== 0)
- {
-  digitalWrite(D4 , LOW);
-  delay(500);
-}
+ else
+ if(RS<500 and ERS >500) { //For curve right turn
+
+ analogWrite(mLf , 200) ;
+ digitalWrite(mRf , LOW) ;
+ digitalWrite(mLb , LOW) ;
+ digitalWrite(mRb , LOW) ;  
+ }
+ else
+ if(RS<500 and ERS<500) { // For sharp right turn
+
+ analogWrite(mLf , 100) ;
+ digitalWrite(mRf , LOW) ;
+ digitalWrite(mLb, LOW) ;
+ digitalWrite(mRb, LOW) ;
+   
+ }
+  else
+  if() { // For any break in Line( Keep on moving ahead) ;
+
+ digitalWrite(mLf , HIGH) ;
+ digitalWrite(mRf , HIGH) ;
+ digitalWrite(mLb, LOW) ;
+ digitalWrite(mRb, LOW) ;} 
+
+if(sensor2 < 500 and sensor3 < 500 and sensor4 < 500) // Go straight again
+ { 
+  digitalWrite(mLf , HIGH) ;
+  digitalWrite(mRf , HIGH) ;
+  digitalWrite(mLb, LOW) ;
+  digitalWrite(mRb, LOW) ;
+ }
+delay(5);  
 }
